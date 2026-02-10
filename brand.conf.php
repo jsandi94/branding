badfe7faf39779af2f1f2ab8ca09ce953514f455
jsandi94 <jason020694_@hotmail.com>
2026-02-09 18:12:38 -0600

    Add files via upload

---
+++ brand.conf.php
@@ -0,0 +1,8 @@
+<?php
+return [
+        'BRAND_LOGO' => './my_images/cdp_logo.png',
+        'BRAND_LOGO_SIDEBAR' => './my_images/cdp_logo_sidebar.png',
+        'BRAND_LOGO_SIDEBAR_COMPACT' => './my_images/cdp_logo_sidebar_compact.png',
+        'BRAND_FOOTER' => '© CDP Energy',
+        'BRAND_HELP_URL' => 'https://cdpenergy.com'
+];
